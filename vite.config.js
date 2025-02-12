75c3588dcb37acf317368b51c2a8a5d4d2c46684
strekvladgit <strek.vlad@yandex.ru>
2025-02-12 07:04:46 +0300

    rollupoptions

--- vite.config.js
+++ vite.config.js
@@ -10,12 +10,6 @@ export default {
     rollupOptions: {
       input: {
         index: resolve(__dirname, './index.html'),
-        register: resolve(__dirname, './pages/register/register.html'),
-        login: resolve(__dirname, './pages/login/login.html'),
-        404: resolve(__dirname, './pages/errors/page4xx/page4xx.html'),
-        500: resolve(__dirname, './pages/errors/page5xx/page5xx.html'),
-        profile: resolve(__dirname, './pages/profile/profile.html'),
-        chat: resolve(__dirname, './pages/chat/chat.html'),
       },
     },
   },
