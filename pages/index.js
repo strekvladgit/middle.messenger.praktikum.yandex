35fd1aa1e8b7cf1d596c809d7bce273c020fe014
strekvladgit <strek.vlad@yandex.ru>
2025-02-13 22:50:57 +0300

    scroll-messages

--- pages/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Chat } from './chat/Chat.ts';
-export { default as Login } from './login/Login.ts';
-export { default as Register } from './register/Register.ts';
-export { default as PagesList } from './pagesList/pagesList.ts';
-export { default as Profile } from './profile/Profile.ts';
-export { default as Page404 } from './errors/page4xx/Page404.ts';
